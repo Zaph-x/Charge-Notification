cf46cd26050b619c7d34f8b4e335231410e5bb13
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2023-01-27 13:42:55 +0100

    Initial commit

---
+++ battery-notification.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+# battery-notification.sh
+# Author: @zaph-x
+# Description: A simple script to notify the user when the battery is low
+# Dependencies: notify-send
+# Usage: ./battery-notification.sh
+
+
+# calculate percentage
+
+export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=/run/user/${UID}/bus}"
+export XDG_RUNTIME_DIR=/run/user/1000
+
+
+PERCENTAGE=$(echo "scale=4; $(cat /sys/class/power_supply/BAT*/energy_now | awk '{ s+=$1 } END { print s }')/$(cat /sys/class/power_supply/BAT*/energy_full | awk '{ s+=$1 } END { print s }') * 100" | bc)
+
+# substring the last two digits
+PERCENTAGE=${PERCENTAGE%00}
+
+# notify if battery is low
+if [ $(echo "$PERCENTAGE < 20" | bc) -eq 1 ]; then
+    notify-send "Battery low" "Battery is at $PERCENTAGE%"
+    paplay /usr/share/sounds/freedesktop/stereo/window-attention.oga
+fi
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+
+# Check if the user is root
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+# check if folder exists
+if [ ! -d "/usr/share/sounds/freedesktop/stereo/" ]; then
+    echo "Creating folder /usr/share/sounds/freedesktop/stereo/"
+    mkdir -p /usr/share/sounds/freedesktop/stereo/
+fi
+
+if [ -f /usr/share/sounds/freedesktop/stereo/window-attention.oga ]; then
+    echo "Sound file exists"
+else
+    echo "Sound file does not exist"
+    echo "Copying sound file"
+    cp window-attention.oga /usr/share/sounds/freedesktop/stereo/
+fi
+
+
+# Install the package
+install battery-notification.sh /usr/bin/battery-notification.sh
+
+cp battery-notification.service /etc/systemd/system/ && sudo systemctl enable battery-notification.service && sudo systemctl start battery-notification.service
+cp battery-notification.timer /etc/systemd/system/ && sudo systemctl enable battery-notification.timer && sudo systemctl start battery-notification.timer
+
+echo "Service: $(systemctl is-active battery-notification.service)"
+echo "Timer: $(systemctl is-active battery-notification.timer)"
